5fba7ccc0ee7f02cde6b7543073e41f266b0a4a2
crashoz <crashoz@server.fake>
2013-08-14 21:30:33 +0200

    now handles last.fm api, can scrobble

--- oz-scrobbler.user.js
+++ oz-scrobbler.user.js
@@ -5,9 +5,25 @@
 // @include     http://*.youtube.com/watch?*v=*
 // @include     https://*.youtube.com/watch?*v=*
 // @version     0.1
+// @require     http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/md5.js
+// @grant       GM_log
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant		GM_deleteValue
+// @grant       GM_xmlhttpRequest
+// @grant       GM_openInTab
 // ==/UserScript==
 
-const UID = 4628736146;
+const UID = "4628736146";
+const lastFmAuth = "http://www.last.fm/api/auth"
+const lastFmApi = "http://ws.audioscrobbler.com";
+const apiKey = "";
+const secretKey = "";
+
+var authToken = "";
+var sessionKey = "";
+var userName = "";
+var hasScrobbled = false;
 
 function debug(text) {
     GM_log(text);
@@ -91,7 +107,7 @@ function parseVideoDuration(durationString) {
  *  Search for 'artist - title', otherwise let the user choose.
  */
 function parseVideoTitle(title) {
-    var reg = /([^\-]+)-(.+)/m;
+    var reg = /([^\-]+) *- *(.+)/m;
     var match = reg.exec(title);
     
     if (match) {
@@ -165,10 +181,10 @@ function addScrobblePanel() {
     '<tr>'+
     '<td><strong>Scrobble to last.fm</strong></td>'+
     '<td></td>'+
-    '<td><button class="yt-uix-button yt-uix-button-text yt-uix-button-size-default" id="advanced-button-' + UID +
+    '<td><button class="yt-uix-button yt-uix-button-text yt-uix-button-size-default" id="login-button-' + UID +
     '" role="button" type="button" onclick=";return false;">'+
     '<span class="yt-uix-button-content">'+
-    'options'+
+    'login'+
     '</span>'+
     '</button></td>'+
     '<tr>'+
@@ -202,8 +218,8 @@ function addScrobblePanel() {
     
     parentElement.insertBefore(mainDiv, insertElement);
     
-    var advancedButton = document.getElementById('advanced-button-' + UID);
-    advancedButton.addEventListener('click', toggleAdvancedOptions, true);
+    var advancedButton = document.getElementById('login-button-' + UID);
+    advancedButton.addEventListener('click', onLogin, true);
     
     var scrobbleButton = document.getElementById('scrobble-button-' + UID);
     scrobbleButton.addEventListener('click', onScrobble, true);
@@ -250,17 +266,238 @@ function onScrobble() {
     '</span>'+
     '</p>';
     
-    var title = document.getElementById('scrobble-title-' + UID).value;
-    var artist = document.getElementById('scrobble-artist-' + UID).value;
+    if (!hasScrobbled) {
+        scrobbleToLastFm();
+    } else {
+        displayError("already scrobbled");
+    }
 }
 
 /**
- *  Toggle the advanced options in the scrobble panel
+ *  Init login options in the scrobble panel
  */
-function toggleAdvancedOptions() {
-    debug("toggling advanced options");
-    var advancedDiv = document.getElementById('scrobble-advanced-' + UID);
-    advancedDiv.classList.toggle('hid');
+function onLogin() {
+    debug("logging in");
+    if (!loadLastFmSessionKey()) {
+        if (!loadLastFmAuthToken()) {
+            getLastFmAuthToken();
+        } else {
+            requestLastFmSessionKey();
+        }
+    } else {
+        displaySuccess("connected as " + userName);
+    }
+}
+
+/**
+ *  Desktop authentication:
+ *  Request an authentication token for api requests.
+ *  The token is valid for 60 minutes, so we can keep it locally in cache
+ *  to avoid unecessary requests.
+ */
+function getLastFmAuthToken() {
+    debug("requesting token");
+    GM_xmlhttpRequest({
+        method: "GET",
+        url: lastFmApi + "/2.0/?method=auth.gettoken&api_key=" + apiKey + "&format=json",
+        timeout: 5000,
+        onload: function(response) {
+            try {
+                authToken = JSON.parse(response.responseText).token;
+                if (!authToken) {
+                    authError(); 
+                } else {
+                    debug("token loaded from request: " + authToken);
+                    
+                    GM_setValue("authTokenTimeout", parseInt(Date.now() / 1000 + 3300));
+                    GM_setValue("authToken", authToken);
+                    
+                    requestLastFmSessionKey();
+                }
+            } catch (e) {
+                authError();
+            }
+        },
+        onerror: authError
+    });
+}
+
+/**
+ *  Try to load the auth token from cache.
+ */
+function loadLastFmAuthToken() {
+    var authTokenTimeout = GM_getValue("authTokenTimeout");
+    if (authTokenTimeout && Date.now() / 1000 < authTokenTimeout ) {
+        authToken = GM_getValue("authToken");
+        if (authToken) {
+            debug("token loaded from cache: " + authToken);
+            return true; 
+        }
+    }
+    
+    return false;
+}
+
+/**
+ *  Desktop authentication:
+ *  Get user last.fm session key. 
+ *  The key has infinite lifetime so we store it locally.
+ */
+function requestLastFmSessionKey() {
+    debug("requesting session key");
+    var sig = CryptoJS.MD5("api_key" + apiKey + "methodauth.getSessiontoken" + authToken + secretKey);
+    GM_xmlhttpRequest({
+        method: "GET",
+        url: lastFmApi + "/2.0/?method=auth.getSession&api_key=" + apiKey + "&api_sig=" + sig + "&token=" + authToken + "&format=json",
+        timeout: 5000,
+        onload: function(response) {
+            try {
+                var json = JSON.parse(response.responseText)
+                if (json.error) {
+                    if (json.error == 14) {
+                        // This token has not been authorized
+                        openLastFmAuthPage();
+                    } else if (json.error == 4) {
+                        // Invalid authentication token supplied
+                        getLastFmAuthToken();
+                    } else {
+                        authError();
+                    }
+                } else {
+                    sessionKey = json.session.key;
+                    userName = json.session.name;
+                    debug("sessionKey loaded from request: " + sessionKey);
+                    // Auth token is destroyed on getSession success
+                    authToken = "";
+                    authTokenTimeout = 0;
+                    GM_setValue("authToken", authToken);
+                    GM_setValue("authTokenTimeout", authTokenTimeout);
+
+                    GM_setValue("sessionKey", sessionKey);
+                    GM_setValue("userName", userName);
+                    
+                    displaySuccess("connected as " + userName);
+                }
+            } catch (e) {
+                authError();
+            }
+        },
+        onerror: authError
+    });
+}
+
+/**
+ *  Try to load the session key from cache.
+ *  If it fails, it means we need to authenticate the user.
+ */
+function loadLastFmSessionKey() {
+    sessionKey = GM_getValue("sessionKey");
+    userName = GM_getValue("userName");
+    if (sessionKey && userName) {
+        debug("sessionKey loaded from cache: " + sessionKey);
+        debug("userName loaded from cache: " + userName);
+        return true; 
+    } else {
+        return false;
+    }
+}
+
+/**
+ *  Send the user to last.fm authentication page to authorize the scrobbler.
+ */
+function openLastFmAuthPage() {
+    displayError("reload page when the scrobbler is authorized");
+    GM_openInTab(lastFmAuth + "/?api_key=" + apiKey + "&token=" + authToken);
+}
+
+function authError() {
+    displayError("authentication error");
+}
+
+/**
+ *  Scrobble the song
+ */
+function scrobbleToLastFm() {
+    debug("requesting scrobble");
+    var artist = document.getElementById('scrobble-artist-' + UID).value;
+    var title = document.getElementById('scrobble-title-' + UID).value;
+    var timestamp = Math.round(Date.now() / 1000);
+    
+    if (!sessionKey) {
+        displayError("Please log in");
+    }
+    
+    if (!artist || !title) {
+        displayError("Missing artist or title");
+    }
+    
+    var s = "api_key" + apiKey + "artist" + artist + "method" + "track.scrobble" + "sk" + sessionKey + "timestamp" + timestamp + "track" + title + secretKey;
+    
+    var sig = CryptoJS.MD5(s);
+    var args = "method=" + encodeURIComponent("track.scrobble") +
+    "&api_key=" + encodeURIComponent(apiKey) +
+    "&api_sig=" + encodeURIComponent(sig) +
+    "&artist=" + encodeURIComponent(artist) +
+    "&track=" + encodeURIComponent(title) +
+    "&timestamp=" + encodeURIComponent(timestamp) +
+    "&sk=" + encodeURIComponent(sessionKey);
+    GM_xmlhttpRequest({
+        method: "POST",
+        url: lastFmApi + "/2.0/?format=json",
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        data: args,
+        timeout: 5000,
+        onload: function(response) {
+            debug(response.responseText);
+            try {
+                var json = JSON.parse(response.responseText)
+            } catch (e) {
+                displayError("Scrobble error - internal error");
+                return;
+            }
+            
+            debug(json);
+            
+            if (json.error) {
+                if (json.error == 14) {
+                    // This token has not been authorized
+                    displayError("Not logged in - Please log in");
+                } else if (json.error == 4) {
+                    // Invalid authentication token supplied
+                    displayError("Not logged in - Please log in");
+                } else if (json.error == 9) {
+                    // Invalid session key - Please re-authenticate
+                    sessionKey = "";
+                    userName = "";
+                    GM_setValue("sessionKey", "");
+                    GM_setValue("userName", "");
+                    displayError("Invalid session key - Please re-authenticate");
+                } else {
+                    // Other
+                    displayError("Scrobble error - Network or application error");
+                }
+            } else if (json.scrobbles['@attr'].accepted == 1) {
+                debug("scrobble successfull");
+                hasScrobbled = true;
+                displaySuccess("Scrobble successful");
+            } else {
+                displayError("Scrobble error - bad metadata");
+            }
+        },
+        onerror: function(response) {
+            displayError("Scrobble error");
+        }
+    });
+}
+
+function displayError(text) {
+    var infoDiv = document.getElementById('scrobble-info-' + UID);
+    infoDiv.innerHTML = '<p style="color:#ff0000;">' + text + '</p>';
+}
+
+function displaySuccess(text) {
+    var infoDiv = document.getElementById('scrobble-info-' + UID);
+    infoDiv.innerHTML = '<p style="color:#00ff00;">' + text + '</p>';
 }
 
 function main() {
@@ -281,6 +518,18 @@ function main() {
         document.getElementById('scrobble-title-' + UID).value = songInfo.title;
         document.getElementById('scrobble-artist-' + UID).value = songInfo.artist
     }
+    
+    if (loadLastFmSessionKey()) {
+        displaySuccess("connected as " + userName);
+    } else {
+        displayError("not logged in");
+    }
 }
 
-main();
+// Avoid running multiple times in the same page (iframes, ...)
+if (window.top != window.self)
+{
+    return;
+} else {
+    window.addEventListener("load", main, false);
+}
